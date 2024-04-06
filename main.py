2490c55a6dff16769b0d510c4c9f0eec6e7facef
LH <70417546+LMileishkaH@users.noreply.github.com>
2024-04-06 01:30:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("~Tunnels~")
+
+start = input("Wanna play? (y/n): ")
+
+if start == "y" or start == "Y":
+  print("Okay, let's start!")
+  print("You are in a dark tunnel. You can go left or right.") 
+  direction = input("Which way do you go? (l/r): ")
+
+  if direction == "l":
+    print("You got to the left and a huge snake wraps around you and squeezes til your eyes pop out of its sockets. You died.")
+
+
+
+
+
+
+else: 
+  print("Okay, bye!")
